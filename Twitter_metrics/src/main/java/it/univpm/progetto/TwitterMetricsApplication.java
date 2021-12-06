a75bbb285baeefd10fb0afce0efd8b04cf13e7e6
ivanpacenti <s1103326@studenti.univpm.it>
2021-12-06 20:53:34 +0100

    prima commit

---
+++ Twitter_metrics/src/main/java/it/univpm/progetto/TwitterMetricsApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterMetricsApplication.class, args);
+	}
+
+}
---
+++ Twitter_metrics/src/test/java/it/univpm/progetto/TwitterMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univpm.progetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
