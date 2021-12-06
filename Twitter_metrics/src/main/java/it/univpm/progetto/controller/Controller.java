514d0ab40eda7565f0c96fb8e45a218c10171f25
ivanpacenti <91594768+ivanpacenti@users.noreply.github.com>
2021-12-06 21:05:56 +0100

    seconda commit

---
+++ Twitter_metrics/src/main/java/it/univpm/progetto/controller/Controller.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package it.univpm.progetto.controller;
+
+import java.util.Vector;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ivan
+ *
+ */
+
+public class Controller {
+	
+}
